69b2d9fcea6cf7da6fbbd71198680def10a90ea9
PIETRO-ing <pa.pietro81@gmail.com>
2021-10-26 22:38:49 +0200

    queries folder

---
+++ queries/queries.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+import time
+
+time.sleep(300)
+print('Hello!\nConnecting!')
+# make the connection
+pg = create_engine('postgresql://postgres:pwd@postgresdb/company', echo=True)
+
+query_00 = """select * 
+              from regions;"""
+df_00 = pd.read_sql(query_00, pg)
+print(df_00)
+
+query_01 = """select * from employees
+               where salary > 100000
+               order by salary desc
+               limit 5;"""
+df_01 = pd.read_sql(query_01, pg)
+print(df_01)
+
